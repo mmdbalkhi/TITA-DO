8d2cb18150a69be2c3704b455ee10431592aa806
KomeilParseh <ahmdparsh129@gmail.com>
2020-11-12 20:00:44 +0330

    Enigma Encode and Decode last version!

--- moudul/enigma.py
+++ moudul/enigma.py
@@ -1,28 +1,38 @@
 """crate with python3, this is a moduls tita do, Enigma simulator! crptugraphi for data!"""
-from pickle import load, dump
-import random
+from random import shuffle
+from pickle import dump, load
 
 alphabet = '''abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 ~`!@#$%^&*()-_=+\|}]{["':;?/>.<,'''
 cipher = ''''''
 
 
-File = open('./Data/Enigma.Eng', 'ab')
-r1 = list(alphabet)
-random.shuffle(r1)
-r1 = ''.join(r1)
-r2 = list(alphabet)
-random.shuffle(r2)
-r2 = ''.join(r2)
+def rotor_genarator():
+    r1 = list(alphabet)
+    shuffle(r1)
+    r1 = ''.join(r1)
+    
+    r2 = list(alphabet)
+    shuffle(r2)
+    r2 = ''.join(r2)
+    
+    r3 = list(alphabet)
+    shuffle(r3)
+    r3 = ''.join(r3)
+    
+    File=open('./Data/Enigma.Tita', 'ab')
+    
+    dump((r1, r2, r3), File)
+    
+    File.close()
 
-r3 = list(alphabet)
-random.shuffle(r3)
-r3 = ''.join(r3)
-dump((r1, r2, r3), File)
-File.close()
+rotor_genarator()
 
-file = open("./Data/Enigma.Eng", "rb")
+
+file = open('./Data/Enigma.Tita', 'rb')
 Rotor1, Rotor2, Rotor3 = load(file)
 file.close()
+#print ('rotor is ....',Rotor1,'  ' ,Rotor2,'  ' ,Rotor3)
+#print(len(alphabet))
 
 def reflector(char):
     return alphabet[len(alphabet)-alphabet.find(char)-1]
@@ -38,6 +48,7 @@ def enigma_one_char(char):
 
     return char1
 
+state = 0
 def rotate_rotors():
     global Rotor1, Rotor2, Rotor3
     Rotor1 = Rotor1[1:] + Rotor1[0]
@@ -46,15 +57,13 @@ def rotate_rotors():
     if state % (26 * 26):
         Rotor3 = Rotor3[1:] + Rotor3[0]
 
-state = 0
 
 def Enigma_starter(plain):
-    """Enigma Starter! Enigma Simulator"""
+    global state, cipher
     for char in plain:
-        global state, cipher
         state += 1
         cipher += enigma_one_char(char)
         rotate_rotors()
     
     # that's print!
-    return cipher
+    return(cipher)

4f132ba3f9bc57b650c9885acd2dac74d22aec2b
KomeilParseh <ahmdparsh129@gmail.com>
2020-11-14 13:45:32 +0330

    enigma edit for save_data suported!

--- moudul/save_data.py
+++ moudul/save_data.py
@@ -1,27 +1,27 @@
 """save data data in TITA-DO project
 
-if exists('../Data/Data.tita') == True:
-    with open("name.log", "r") as file:
-        pass
-
-if exists('name.log') == False:
-    with open("name.log", "w") as file:
-        pass"""
-#from os.path import exists
-#from sys import exit
+"""
+from os.path import exists
+from sys import exit
 print("please Enter alphabet a short!")
 
 while True:
     todo = str(input('YourTodo?'))
+
+    file = open("Data.tita')", "a")
+    file.write(todo)
+    file.write('\n')
     if todo == 'exit':
         close = str(input('Ary you sure?'))
         if close == 'ok':
             print('goodbye!')
-            break
+            exit()
     elif todo == 'Show now':
         print('showed!')
         print(todo)
-        #break
+        if exists('../Data/Data.tita') == True:
+            with open("Data.tita')", "r") as file:
+                print(file.read())
     else:
         print("hello!")
         #break

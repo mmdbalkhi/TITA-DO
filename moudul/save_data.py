6b09ff84a67d3a3c915929753f915749dc533772
KomeilParseh <ahmdparsh129@gmail.com>
2020-11-27 20:04:42 +0330

    save data moudo

--- moudul/save_data.py
+++ moudul/save_data.py
@@ -3,11 +3,12 @@
 from os.path import exists
 from sys import exit
 from enigma import Enigma_starter
+
 print("please Enter alphabet a short!")
 number = 0
 while True:
     todo = str(input('YourTodo?'))
-    if todo != 'exit' and todo != 'Show now':
+    if todo != 'exit' and todo != 'show now':
         print(todo)
         number += 1
         file = open("Data.tita", "a")
@@ -15,8 +16,8 @@ while True:
         file.writelines('\n')
         file.close()
     if todo == 'exit':
-        close = str(input('Ary you sure?\n \t |yes is ok| \n\t |no is no!|\n'))
-        if close == 'ok':
+        close = str(input('Ary you sure? yes or no? \n'))
+        if close == 'yes':
             print('goodbye!')
             exit()
     if todo == 'Show now':

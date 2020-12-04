cb737d1842bb4c8d0c15edb401fcac1b75c7c3bd
KomeilParseh <ahmdparsh@gmail.com>
2020-12-04 15:31:49 +0330

    web app change

--- TITADOweb/web/models.py
+++ TITADOweb/web/models.py
@@ -1,3 +1,30 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Token(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=48)
+
+
+class TODO(models.Model):
+    TODO = models.CharField(max_length=250)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.TODO
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    def __unicode__(self):
+        return "{}-{}".format(self.pub_date, self.TODO)
+
+
+class Choice(models.Model):
+    TODO = models.ForeignKey(TODO, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
--- TITADOweb/web/views.py
+++ TITADOweb/web/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def submit_TODO(request):
+    """User submit and request"""
+    print("we are hare")

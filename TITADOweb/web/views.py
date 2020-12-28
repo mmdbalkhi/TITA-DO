e8556b2a505bad3ca8a7bf956257b001d81ac137
KomeilParseh <ahmdparsh@gmail.com>
2020-12-28 13:44:49 +0330

    fix bugs

--- TITADOweb/web/views.py
+++ TITADOweb/web/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from json import encoder
 # Create your views here.
 
+@csrf_exempt
+def Todo(request):
+    print(request.POST)
+
+    return JsonResponse({
+        'status' : 'lk',
+    }, encoder = encoder)
 
 def index(request):
     context = {}

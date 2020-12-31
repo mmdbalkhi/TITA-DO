0cd2925442a6f781a6326dc9ed18a74fc6116f6e
KomeilParseh <ahmdparsh@gmail.com>
2020-12-31 14:57:04 +0330

    Time zone For iran

--- TITADOweb/TITADOweb/settings.py
+++ TITADOweb/TITADOweb/settings.py
@@ -106,15 +106,14 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tehran'
 
 USE_I18N = True
 
 USE_L10N = True
 
 USE_TZ = True
-
-
+APPEND_SLASH=False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 

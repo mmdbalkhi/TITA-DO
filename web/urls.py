2224d3188a7dd9a80a2e4a111fcd2220c6bb4f80
KomeilParseh <ahmdparsh@gmail.com>
2021-01-01 18:54:21 +0330

    edit urls in web app

--- web/urls.py
+++ web/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    #url(r'^postTodo/?$', views.postTodo, name = 'postTodo'),
-    url(r'^TodoPages$', views.TodoPage, name='TodoPages'),
+    url(r'^postTodo/?$', views.postTodo, name = 'postTodo'),
     url(r'^TodoPages/$', views.TodoPage, name='TodoPages'),
 ]

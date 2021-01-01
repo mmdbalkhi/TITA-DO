1a689a65072ce191008fc701948788b5232f2980
KomeilParseh <ahmdparsh@gmail.com>
2021-01-01 09:39:49 +0330

    fix bugs and bested

--- web/urls.py
+++ web/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^postTodo/?$', views.postTodo, name = 'postTodo'),
+    #url(r'^postTodo/?$', views.postTodo, name = 'postTodo'),
+    url(r'^TodoPages$', views.TodoPage, name='TodoPages'),
+    url(r'^TodoPages/$', views.TodoPage, name='TodoPages'),
 ]
--- web/views.py
+++ web/views.py
@@ -1,38 +1,38 @@
 from json import JSONEncoder
 
 from django.http import JsonResponse
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render
 from django.utils import timezone
 from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_POST
-
+from datetime import datetime
 from web.models import Todos, Token, User
-
 # Create your views here.
 
 
 # submit an expanse to system (api) , input : token(POST) , output :
 # status = (ok)
 @csrf_exempt
-@require_POST
 def postTodo(request):
     """ submit an Todo """
 
-    # TODO: revise validation for the amount
-    
-    this_date = request.POST['date'] if 'date' in request.POST else timezone.now()
-    this_text = request.POST['text'] if 'text' in request.POST else ""
-    this_todo = request.POST['todo'] if 'todo' in request.POST else ""
-    this_token = request.POST['token'] if 'token' in request.POST else ""
-    CheckBox = request.POST['checkBox'] if 'checbox' in request.POST else ""
-    this_user = get_object_or_404(User, token__token=this_token)
+    try:# TODO: not working in new version
+        this_token = request.POST['token']
+        this_user = User.objects.filter(token__token = this_token.get())
+        now = datetime.now()
+        Todos.object.create(user = this_user, todo = request.POST['todo'],
+                            text = request.POST['text'], date = now)
+        return JsonResponse({
+            'status': 'ok',
+        }, encoder=JSONEncoder)  # return {'status':'ok'}
 
-    Todos.objects.create(user=this_user, todo=this_todo,
-                           text=this_text, date=this_date)
+    except:
+        return JsonResponse({
+            'status': 'No',
+        }, encoder=JSONEncoder)  # return {'status':'No'}
 
-    return JsonResponse({
-        'status': 'ok',
-    }, encoder=JSONEncoder)  # return {'status':'ok'}
+def TodoPage(request):
+    context = {}
+    return render(request, 'TodoPages.html', context)
 
 
 def index(request):
